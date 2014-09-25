ad265d66b907adfd39e6d605d6614e52ba757d13
Cyril Rohr <cyril.rohr@gmail.com>
2014-09-25 20:05:36 +0000

    tests

---
+++ test/env.sh
@@ -0,0 +1,8 @@
+export APP_USER=nobody
+export APP_GROUP=nobody
+export APP_NAME=blank-noop-app
+export APP_SAFE_NAME=blank-noop-app
+export APP_HOME=/tmp
+export INSTALLER_DIR=$(pwd)
+export INSTALLER_DEBUG="yes"
+export ADDONS="mysql apache2"
