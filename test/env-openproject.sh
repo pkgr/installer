a8aa842b60f276cd6a9c6dde075f346dd32c99ae
Cyril Rohr <cyril.rohr@gmail.com>
2014-09-26 15:18:27 +0000

    add env for openproject

---
+++ test/env-openproject.sh
@@ -0,0 +1,8 @@
+export APP_USER=someuser
+export APP_GROUP=somegroup
+export APP_NAME=openproject
+export APP_SAFE_NAME=openproject
+export APP_HOME=/opt/openproject
+export INSTALLER_DIR=$(pwd)
+export INSTALLER_DEBUG="yes"
+export ADDONS="mysql apache2 smtp"
