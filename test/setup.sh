fd0369bf1a06d3064fab6af959f2dd7bf6b4cca8
Cyril Rohr <cyril.rohr@gmail.com>
2015-01-15 00:30:54 +0000

    Update for SUSE

--- test/setup.sh
+++ test/setup.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
+BRANCH=${BRANCH:="master"}
+
 set -ex
 
-addons="mysql apache2 smtp svn-dav memcached"
+addons="mysql apache2 smtp svn-dav memcached openproject"
 for addon in $addons ; do
   rm -rf addons/$addon
-  git clone https://github.com/pkgr/addon-$addon --branch installer addons/$addon
+  git clone https://github.com/pkgr/addon-$addon --branch ${BRANCH} addons/$addon
 done
