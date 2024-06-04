9a186c0b064100eabe2b19ccdb23fce0239321a7
Christophe Bliard <c.bliard@openproject.com>
2024-06-04 16:48:01 +0200

    Make template dynamic based on the value of the key
    It's possible to define additional versions of a template based on the
    current value of the key. This allows for more dynamic templates based
    on the previous user's input. For instance it can be used to indicate
    that a server would be uninstalled if "skip" is chosen during a
    reconfiguration.
    
    Also:
    - Change assert output to make diff easier to reader
    - Keep paragraphs in the description: separate them with 2 newlines
      instead of only 1.

--- test/wiz_meta.sh
+++ test/wiz_meta.sh
@@ -1,6 +1,7 @@
 #!/bin/bash -e
 
 DIR=$(dirname "$0")
+export DATABASE=$(mktemp)
 
 . ${DIR}/assert.sh
 . $(dirname "$DIR")/wizard
@@ -32,6 +33,15 @@ test_choices() {
 assert "test_type" "select"
 assert "test_title" "Do you want to use this wizard to help setup your MySQL database?"
 assert "test_default" "skip"
-assert "test_description" "If you want, we can automatically create the MySQL database required by _APP_NAME_. If you choose NOT to use this wizard, you will have to manually setup all the things related to the database."
-assert "test_choices" '"skip" "Skip this wizard altogether" "use an existing database" "Use an existing MySQL database" "create a new database " "Create a new database (requires MySQL superuser password)"' 
+assert "test_description" "If you want, we can automatically create the MySQL database required by _APP_NAME_.\\\n\\\nIf you choose NOT to use this wizard, you will have to manually setup all the things related to the database."
+assert "test_choices" '"skip" "Skip this wizard altogether" "use an existing database" "Use an existing MySQL database" "create a new database" "Create a new database (requires MySQL superuser password)"'
+
+# template can be different depending on the current value
+wiz_set "mysql/autoinstall" "use an existing database"
+assert "test_type" "select"
+assert "test_title" "Do you want to keep your existing database?"
+assert "test_default" "use an existing database"
+assert "test_description" "You previously chose to use your existing MySQL database.\\\n\\\nIf you want, we can automatically create the MySQL database required by _APP_NAME_.\\\n\\\nIf you choose NOT to use this wizard, you will have to manually setup all the things related to the database."
+assert "test_choices" '"skip" "Skip this wizard altogether" "use an existing database" "Keep existing MySQL database" "create a new database" "Create a new database (requires MySQL superuser password)"'
+
 assert_end "wiz_meta"
